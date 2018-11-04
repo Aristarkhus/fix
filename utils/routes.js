b91fdf60c349ad6297a06212ba6f584f33dd9a49
alvinwicaksono <alvin.wicaksono@ti.ukdw.ac.id>
2018-11-05 01:44:27 +0700

    error handling js

--- utils/routes.js
+++ utils/routes.js
@@ -13,21 +13,21 @@ class Routes{
 
 		this.app = app;
 	}
-	
+
 	appRoutes(){
 		this.app.post('/usernameCheck',async (request,response) =>{
 			const username = request.body.username;
 			if (username === "" || username === undefined || username === null) {
 				response.status(412).json({
 					error : true,
-					message : `username cant be empty.`
+					message : `Username masih kosong.`
 				});
 			} else {
 				const data = await helper.userNameCheck(username.toLowerCase());
 				if (data[0]['count'] > 0) {
 					response.status(401).json({
 						error:true,
-						message: 'This username is alreday taken.'
+						message: 'Username sudah digunakan.'
 					});
 				} else {
 					response.status(200).json({
@@ -36,32 +36,32 @@ class Routes{
 					});
 				}
 			}
-		});		
+		});
 
 		this.app.post('/registerUser', async (request,response) => {
 			const registrationResponse = {}
 			const data = {
 				username : (request.body.username).toLowerCase(),
 				password : request.body.password
-			};			
+			};
 			if(data.username === '') {
 	            registrationResponse.error = true;
-	            registrationResponse.message = `username cant be empty.`;
+	            registrationResponse.message = `Username masih kosong.`;
 	            response.status(412).json(registrationResponse);
-	        }else if(data.password === ''){				            
+	        }else if(data.password === ''){
 	            registrationResponse.error = true;
-	            registrationResponse.message = `password cant be empty.`;
+	            registrationResponse.message = `Password masih kosong.`;
 	            response.status(412).json(registrationResponse);
-	        }else{	        	
+	        }else{
 				const result = await helper.registerUser( data );
 				if (result === null) {
 					registrationResponse.error = true;
-					registrationResponse.message = `User registration unsuccessful,try after some time.`;
+					registrationResponse.message = `Registrasi gagal.`;
 					response.status(417).json(registrationResponse);
 				} else {
 					registrationResponse.error = false;
 					registrationResponse.userId = result.insertId;
-					registrationResponse.message = `User registration successful.`;
+					registrationResponse.message = `Hore!!. Registrasi Berhasil.`;
 					response.status(200).json(registrationResponse);
 				}
 	        }
@@ -75,17 +75,17 @@ class Routes{
 			};
 			if(data.username === '' || data.username === null) {
 	            loginResponse.error = true;
-	            loginResponse.message = `username cant be empty.`;
+	            loginResponse.message = `Username tidak boleh kosong.`;
 	            response.status(412).json(loginResponse);
-	        }else if(data.password === '' || data.password === null){				            
+	        }else if(data.password === '' || data.password === null){
 	            loginResponse.error = true;
-	            loginResponse.message = `password cant be empty.`;
+	            loginResponse.message = `Password tidak boleh kosong.`;
 	            response.status(412).json(loginResponse);
 	        }else{
 				const result = await helper.loginUser(data);
 				if (result === null || result.length === 0) {
 					loginResponse.error = true;
-					loginResponse.message = `Invalid username and password combination.`;
+					loginResponse.message = `Username dan Password tidak sesuai.`;
 					response.status(401).json(loginResponse);
 				} else {
 					loginResponse.error = false;
@@ -95,10 +95,10 @@ class Routes{
 				}
 	        }
 		});
-		
+
 		this.app.post('/userSessionCheck', async (request,response) =>{
 			const userId = request.body.userId;
-			const sessionCheckResponse = {}			
+			const sessionCheckResponse = {}
 			if (userId == '') {
 				sessionCheckResponse.error = true;
 	            sessionCheckResponse.message = `User Id cant be empty.`;
@@ -117,11 +117,11 @@ class Routes{
 				}
 	        }
 		});
-		
+
 		this.app.post('/getMessages',async (request,response) => {
 			const userId = request.body.userId;
 			const toUserId = request.body.toUserId;
-			const messages = {}			
+			const messages = {}
 			if (userId === '') {
 				messages.error = true;
 	            messages.message = `userId cant be empty.`;
@@ -139,13 +139,13 @@ class Routes{
 				}
 	        }
 		});
-		
+
 		this.app.get('*',(request,response) =>{
 			response.sendFile(path.join(__dirname + '../../client/views/index.html'));
 			/*
 			* OR one can define the template engine and use response.render();
 			*/
-		});		
+		});
 	}
 
 	routesConfig(){
